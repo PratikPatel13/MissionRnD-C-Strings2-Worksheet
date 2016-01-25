ee700bec71cb5a33917e19ccd4859790d9bec274
PratikPatel13 <PratikPatel13@missionrnd.com>
2016-01-25 21:25:21 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -9,14 +9,43 @@ Output : 4
 
 Note: Dont modify original str or word,Just return count ,Spaces can also be part of words like "ab cd" in "ab cd ab cd" returns 2
 */
-
-#include <stdlib.h>
-
+#include<stdlib.h>
+#include<string.h>
+int compare(char *str, char *word, int i);
 int count_word_in_str_way_1(char *str, char *word){
-	return 0;
+	if (str == NULL || word == NULL)
+		return 0;
+	int i = 0, j = 0, count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == word[j])
+		{
+			if (compare(str, word, i) == 1)
+			{
+				count++;
+			}
+		}
+
+		i++;
+	}
+	return count;
 }
 
-int count_word_int_str_way_2_recursion(char *str, char *word){
-	return 0;
-}
+int compare(char *str, char *word, int i)
+{
+	int j = 0;
+	while (word[j] != '\0')
+	{
+		if (str[i] == word[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return 1;
 
+}
